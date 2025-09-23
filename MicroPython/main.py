709aeada79c78c14b27fb255cfc4fa9fa75135b1
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-09-23 16:31:12 +0000

    changed to kelvin python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -2,7 +2,7 @@
 Created by: Isaac Ip
 Created on: Sep 2025
 This module is a Micro:bit MicroPython program
-This program gets and shows the temperature.
+This program gets and shows the temperature, in K.
 """
 
 from microbit import *
