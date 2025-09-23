8ad1799f54a103aa1fa42fa15b763a1ad70c1e40
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-09-23 12:27:27 -0400

    changed to kelvin

--- main.ts
+++ main.ts
@@ -5,7 +5,20 @@
  * This program gets the current temperature, in K.
 */
 
+// variable
+let temperatureInCelsius: number
+let temperatureInKelvin: number
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// comment
+// when button A is pressed, it shows the temperature
+input.onButtonPressed(Button.A, function () {
+    //show the temperature
+    basic.showString("The temperature is :")
+    temperatureInCelsius = input.temperature()
+    temperatureInKelvin = Math.round(temperatureInCelsius + 273.15)
+    basic.showNumber(temperatureInKelvin)
+    basic.showString("K.")
+    basic.showIcon(IconNames.Happy)
+})
