c22e8866b3fc3cfaf4fff7bf5c1132091c77211b
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-09-23 16:26:35 +0000

    changed to kelvin python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Isaac Ip
+Created on: Sep 2025
 This module is a Micro:bit MicroPython program
+This program gets and shows the temperature.
 """
 
 from microbit import *
+import temperature
 
+display.clear()
+display.show(Image.HAPPY)
 
-display.scroll("Hello, World!")
+while True:
+    if button_a.is_pressed():
+        temperatureInCelsius = temperature()
+        temperatureInKelvin = round(temperatureInKelvin + 273.15)
+        display.show("The temperature is:" + temperatureInKelvin + "K.")
+        display.show(Image.HAPPY)
